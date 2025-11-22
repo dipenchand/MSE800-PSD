bd5049bc6f8f9200298847afccd4e96de322c59b
dipenchand <dipenchand@gmail.com>
2025-11-23 08:40:54 +1300

    initialized week-01

---
+++ week-01/multi_type_profile.py
@@ -0,0 +1,27 @@
+dataType = {
+    "Name": {
+        "Data_Type": "String",
+        "Name": "John Doe",
+    },
+    "Age": {
+        "Data_Type": "Integer",
+        "Name": 28,
+    },
+    "Skills": {
+        "Data_Type": "List",
+        "Name": ["Python", "SQL", "Power BI"],
+    },
+    "Education": {
+        "Data_Type": "Tuple",
+        "Name": ("BSc Computer Science", 2020),
+    },
+    "Contact Details": {
+        "Data_Type": "Dictionary",
+        "Name": {"email": "personaldipen@gmail.com", "phone": "027 437 2485"},
+    },
+    "Certifications": {
+        "Data_Type": "Set",
+        "Name": {"Azure", "AWS", "Azure"},
+    }
+}
+
