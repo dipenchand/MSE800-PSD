95d826d9e316a9abf636231ff4adf984537bb1f5
dipenchand <dipenchand@gmail.com>
2025-11-23 08:55:55 +1300

    finalized Build a Multi-Type Profile

--- week-01/multi_type_profile.py
+++ week-01/multi_type_profile.py
@@ -1,27 +1,37 @@
-dataType = {
+# Initialized dataTypes dictionary variable with values
+dataTypes = {
     "Name": {
         "Data_Type": "String",
-        "Name": "John Doe",
+        "value": "John Doe",
     },
     "Age": {
         "Data_Type": "Integer",
-        "Name": 28,
+        "value": 28,
     },
     "Skills": {
         "Data_Type": "List",
-        "Name": ["Python", "SQL", "Power BI"],
+        "value": ["Python", "SQL", "Power BI"],
     },
     "Education": {
         "Data_Type": "Tuple",
-        "Name": ("BSc Computer Science", 2020),
+        "value": ("BSc Computer Science", 2020),
     },
     "Contact Details": {
         "Data_Type": "Dictionary",
-        "Name": {"email": "personaldipen@gmail.com", "phone": "027 437 2485"},
+        "value": {"email": "personaldipen@gmail.com", "phone": "027 437 2485"},
     },
     "Certifications": {
         "Data_Type": "Set",
-        "Name": {"Azure", "AWS", "Azure"},
+        "value": {"Azure", "AWS", "Azure"},
     }
 }
 
+# Print header with spaces
+print(f"{'Component':<15} {'|':<5} {'Data Type':<10} {'|':<5} {'Example':<10}")
+# Separated header with hyphen with length of 75 characters
+print("-" * 75)
+
+# looping through the dataTypes dictionary
+for key, values in dataTypes.items():
+    # print the key value inside the table body
+    print(f"{key:<20} {values['Data_Type']:<17} {values['value']}")
