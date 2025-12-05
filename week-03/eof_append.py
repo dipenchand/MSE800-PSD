596abb612ea6c448fb8b06ae6494ae51c2b3d1a2
dipenchand <dipenchand@gmail.com>
2025-12-06 09:22:28 +1300

    eof fixed

--- week-03/eof_append.py
+++ week-03/eof_append.py
@@ -6,30 +6,24 @@ class EBookReader:
 
     def display_file_info(self, file_path):
         path = Path(file_path)
-            
-        stat = path.stat()
-        print(f"\nFile Information for '{file_path}':")
-        print(f"  Size: {stat.st_size} bytes")
-        print(f"  Modified: {stat.st_mtime}")
-        print(f"  Is file: {path.is_file()}")
         
         data = open(file_path, 'r')
         lines = data.readlines()
         
-        print("*" * 40)
         for line in lines:
             print(line[0:-1])
         data.close()
     
     # Append content to the end of a file.
-    def append_eof(self, file_path, content_to_append):
+    def append_eof(self, file_path):
+        content_to_append = "**** End of File ****"
         with open(file_path, 'a', encoding='utf-8') as file:
+            file.write("\n")
             file.write(content_to_append)
 
 def main():
     reader = EBookReader()
-    append_content = "**** End of File ****"
-    reader.append_eof("./test_file.txt", append_content)
+    reader.append_eof("./test_file.txt")
     reader.display_file_info("./test_file.txt")
 
 
